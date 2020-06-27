8ac0ba1653d8b9c342255e03d59e9f9924259631
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:56 +1000

    Create YggdrasilResponse.cs

---
+++ src/Yggdrasil/Models/YggdrasilResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Yggdrasil.Models
+{
+    public class YggdrasilResponse<T>
+    {
+        public bool Success { get; set; }
+        public ErrorResponse ErrorResponse { get; set; }
+        public T Response { get; set; }
+    }
+}
