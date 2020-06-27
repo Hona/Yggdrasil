d3e5e16d17267f8946048841bd8d134b590e0e5e
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:42 +1000

    Create RefreshResponse.cs

---
+++ src/Yggdrasil/Models/RefreshResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class RefreshResponse
+    {
+        [JsonProperty("accessToken")] public string AccessToken { get; set; }
+
+        [JsonProperty("clientToken")] public string ClientToken { get; set; }
+
+        [JsonProperty("selectedProfile")] public SlimProfile SelectedProfile { get; set; }
+
+        [JsonProperty("user")] public SlimUser User { get; set; }
+    }
+}
