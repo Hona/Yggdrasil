10c4d81543ce8c82348a25defffbbdac29deda6c
Luke Parker <lukemparker@outlook.com>
2020-06-27 23:08:58 +1000

    Fix deserialisation of errortype

--- src/Yggdrasil/Models/ErrorType.cs
+++ src/Yggdrasil/Models/ErrorType.cs
@@ -1,12 +1,19 @@
-﻿namespace Yggdrasil.Models
+﻿using System.Runtime.Serialization;
+
+namespace Yggdrasil.Models
 {
     public enum ErrorType
     {
         Unknown = 0,
+        [EnumMember(Value = "Method Not Allowed")]
         MethodNotAllowed,
+        [EnumMember(Value = "Not Found")]
         NotFound,
+        [EnumMember(Value = "ForbiddenOperationException")]
         ForbiddenOperationException,
+        [EnumMember(Value = "IllegalArgumentException")]
         IllegalArgumentException,
+        [EnumMember(Value = "Unsupported Media Type")]
         UnsupportedMediaType
     }
 }
