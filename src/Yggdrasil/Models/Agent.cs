a230e47f155f23a18026b8c6d4a397d5f02d483e
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:24 +1000

    Create Agent.cs

---
+++ src/Yggdrasil/Models/Agent.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class Agent
+    {
+        /// <summary>
+        ///     The name of the game
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Typically 1 as set by the vanilla client
+        /// </summary>
+        [JsonProperty("version")]
+        public int Version { get; set; } = 1;
+
+        public static Agent Minecraft => new Agent
+        {
+            Name = "Minecraft",
+            Version = 1
+        };
+    }
+}
