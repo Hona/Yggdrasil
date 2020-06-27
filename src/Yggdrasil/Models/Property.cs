eceac7fa07ccb564aaedff16fb7ad6f28af447d8
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:38 +1000

    Create Property.cs

---
+++ src/Yggdrasil/Models/Property.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class Property
+    {
+        [JsonProperty("name")] public string Name { get; set; }
+
+        [JsonProperty("value")] public string Value { get; set; }
+    }
+}
