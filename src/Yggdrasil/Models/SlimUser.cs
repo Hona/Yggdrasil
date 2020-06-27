de7f59979fee6ae25bb2fc5babcd803a2fa9079e
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:47 +1000

    Create SlimUser.cs

---
+++ src/Yggdrasil/Models/SlimUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class SlimUser
+    {
+        /// <summary>
+        ///     Hex string
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("properties")] public List<Property> Properties { get; set; }
+    }
+}
