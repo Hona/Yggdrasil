39a9898ca8947cefa555f0509adc49636f074cd8
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:33 +1000

    Create ErrorType.cs

---
+++ src/Yggdrasil/Models/ErrorType.cs
@@ -0,0 +1,12 @@
+﻿namespace Yggdrasil.Models
+{
+    public enum ErrorType
+    {
+        Unknown = 0,
+        MethodNotAllowed,
+        NotFound,
+        ForbiddenOperationException,
+        IllegalArgumentException,
+        UnsupportedMediaType
+    }
+}
