80d3a7fd2edcc4f90dbd6a7f9803ca85dac3d81e
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:40 +1000

    Create RefreshPayload.cs

---
+++ src/Yggdrasil/Models/RefreshPayload.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class RefreshPayload
+    {
+        [JsonProperty("accessToken")] public string AccessToken { get; set; }
+
+        [JsonProperty("clientToken")] public string ClientToken { get; set; }
+
+        /// <summary>
+        ///     Optional, sending will result in an error
+        /// </summary>
+        [JsonProperty("selectedProfile")]
+        public SlimProfile SelectedProfile { get; set; }
+
+        /// <summary>
+        ///     Setting this to true adds the user object to the response
+        /// </summary>
+        [JsonProperty("requestUser")]
+        public bool RequestUser { get; set; }
+    }
+}
