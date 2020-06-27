9dc3df9c1be203b3e0eaa2f1f2bcb4d289ab1d52
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:50 +1000

    Create TokensPayload.cs

---
+++ src/Yggdrasil/Models/TokensPayload.cs
@@ -0,0 +1,12 @@
+﻿namespace Yggdrasil.Models
+{
+    public abstract class TokensPayload
+    {
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        ///     Optional, if used, it should be the same as was used to obtain the AccessToken
+        /// </summary>
+        public string ClientToken { get; set; }
+    }
+}
