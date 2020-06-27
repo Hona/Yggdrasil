29e0b4a9becd120ac8dc6d2fb484b8824822ef97
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:27 +1000

    Create AuthenticatePayload.cs

---
+++ src/Yggdrasil/Models/AuthenticatePayload.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class AuthenticatePayload
+    {
+        /// <summary>
+        ///     Use the Agent.Minecraft for Minecraft
+        /// </summary>
+        [JsonProperty("agent")]
+        public Agent Agent { get; set; }
+
+        /// <summary>
+        ///     Use email address, or for unmigrated accounts the username
+        /// </summary>
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        /// <summary>
+        ///     Raw password
+        /// </summary>
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        ///     Optional
+        /// </summary>
+        [JsonProperty("clientToken")]
+        public string ClientToken { get; set; }
+
+        /// <summary>
+        ///     Setting this to true adds the user object to the response
+        /// </summary>
+        [JsonProperty("requestUser")]
+        public bool RequestUser { get; set; }
+    }
+}
