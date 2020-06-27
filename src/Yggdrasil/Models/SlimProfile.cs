9e0559206a06242e283304fc017925e376838ef4
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:45 +1000

    Create SlimProfile.cs

---
+++ src/Yggdrasil/Models/SlimProfile.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class SlimProfile
+    {
+        /// <summary>
+        ///     Stored as a hex string
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        ///     The players name
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
