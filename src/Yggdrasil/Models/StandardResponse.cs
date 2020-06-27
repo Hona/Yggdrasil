9738233b42f5f957d1745f3d68c272d919c68745
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:49 +1000

    Create StandardResponse.cs

---
+++ src/Yggdrasil/Models/StandardResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Yggdrasil.Models
+{
+    /// TODO: `Signout` response + `invalidate` response
+    public enum StandardResponse
+    {
+        Failure = 0,
+        Success = 1
+    }
+}
