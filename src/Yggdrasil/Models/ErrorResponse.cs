7c285c5ad56a0d70eea99d1008c9e3d4ba4e312b
Luke Parker <lukemparker@outlook.com>
2020-06-27 22:37:31 +1000

    Create ErrorResponse.cs

---
+++ src/Yggdrasil/Models/ErrorResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
+{
+    public class ErrorResponse
+    {
+        /// <summary>
+        ///     Short description of the error
+        /// </summary>
+        [JsonProperty("error")]
+        public ErrorType Error { get; set; }
+
+        [JsonProperty("cause")] public string Cause { get; set; }
+
+        /// <summary>
+        ///     Longer description of the error, can be shown to the user
+        /// </summary>
+        [JsonProperty("errorMessage")]
+        public string Message { get; set; }
+    }
+}
