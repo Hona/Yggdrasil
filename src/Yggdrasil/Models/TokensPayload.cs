d290bfd97bc9f24669cfa4e07fc1aab316ce2f9e
Luke Parker <lukemparker@outlook.com>
2020-06-28 12:28:05 +1000

    Fix TokensPayload json serialization

--- src/Yggdrasil/Models/TokensPayload.cs
+++ src/Yggdrasil/Models/TokensPayload.cs
@@ -1,12 +1,16 @@
-﻿namespace Yggdrasil.Models
+﻿using Newtonsoft.Json;
+
+namespace Yggdrasil.Models
 {
     public abstract class TokensPayload
     {
+        [JsonProperty("accessToken")]
         public string AccessToken { get; set; }
 
         /// <summary>
         ///     Optional, if used, it should be the same as was used to obtain the AccessToken
         /// </summary>
+        [JsonProperty("clientToken")]
         public string ClientToken { get; set; }
     }
 }
